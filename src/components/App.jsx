41dca6455f7e901e90a0bd29c7f1243e74b8cf60
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-03-28 15:53:21 +0200

    add phonebox

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Container } from './App.styled';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  takeFormData = ({ name, number }) => {
+    this.setState(prevState => {
+      if (
+        prevState.contacts.some(
+          contact => contact.name.toLowerCase() === name.toLowerCase()
+        )
+      ) {
+        alert(`${name} is already in contacts`);
+        return;
+      }
+      return {
+        contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+      };
+    });
+  };
+  filterChange = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  filterCorrectData = () => {
+    return this.state.contacts.filter(contact => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase());
+    });
+  };
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  render() {
+    const filterContacts = this.filterCorrectData();
+    return (
+      <Container
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h2>Phonebook</h2>
+        <ContactForm onSubmit={this.takeFormData} />
+
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.filterChange} />
+        <ContactList
+          resultData={filterContacts}
+          onDelete={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import { Input, Title } from './Filter.styled';
+import PropTypes from 'prop-types';
+
+export class Filter extends Component {
+  render() {
+    const { onChange, value } = this.props;
+    return (
+      <>
+        <Title>Find contacts by name</Title>
+        <Input type="text" value={value} onChange={onChange} />
+      </>
+    );
+  }
+}
+
+Filter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
